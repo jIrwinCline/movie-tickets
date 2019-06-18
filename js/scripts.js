7e8bf55fe06303bbaf53b9306457e36ccb5db146
Alex Siegberg and Jacob Irwin <jacob@>
2019-06-18 14:45:42 -0700

    add 6 cards to the webpage to display movies

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,10 @@ function Movies() {
   this.currentID = 0;
 }
 
-function Film(name, premier) {
+function Film(name, premier, image) {
   this.name = name;
   this.premier = premier;
+  this.image = image;
 }
 
 Movies.prototype.addfilm = function(film) {
@@ -33,7 +34,8 @@ function displayMovies(showing) {
   var htmlForMovies = "";
 
   showing.films.forEach(function(movie) {
-    htmlForMovies += "<div class='col-md-3' id=" + movie.id + ">" + movie.name + "</div>";
+    htmlForMovies += "<div class='col-md-4' id=" + movie.id + ">" +
+    "<div class='card' style='width: 18rem;'> <img class='card-img-top' src=" + movie.image + " alt=''> <div class='card-body'> <h5 class='card-title'>" + movie.name + "</h5> <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p> <a href='#' class='btn btn-primary'>Link</a></div></div>"+ "</div>";
   });
   movieChoices.html(htmlForMovies);
 }
@@ -54,13 +56,15 @@ function checkMovieCost(ticket) {
 var nowShowing = new Movies;
 
 function makeMovieList() {
-  var ourFilms =[["Toy Story 4", true],
-  ["Avenger's: Endgame", false],
-  ["Child's Play", true],
-  ["Alladin", true]];
+  var ourFilms =[["Toy Story 4", true, "img/toystory4.jpg"],
+  ["Avenger's: Endgame", false, "img/endgame.jpg"],
+  ["Child's Play", true, "img/childsplay.jpg"],
+  ["Alladin", true, "img/alladin.jpg"],
+  ["Godzilla", true, "img/godzilla.jpg"],
+  ["Dark Phoenix", true, "img/darkphoenix.jpg"]];
 
   ourFilms.forEach(function(movie) {
-    var newMovie = new Film(movie[0], movie[1]);
+    var newMovie = new Film(movie[0], movie[1], movie[2]);
     nowShowing.addfilm(newMovie);
   });
 }
